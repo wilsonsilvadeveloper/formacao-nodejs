91d7a2838eeccbbeaad42fef8963b0cd9075efac
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2025-10-22 09:46:10 -0300

    criando uma lista de produtos e usando uma estrutura de repeticao com EJS para visualizar

--- projeto01/index.js
+++ projeto01/index.js
@@ -4,14 +4,23 @@ const port = 3000;
 
 app.set('view engine', 'ejs'); // Define EJS como o motor de visualização
 
-app.get('/:nome/:lang', (req, res) => { // Rota raiz
+app.get('/:nome/:lang/', (req, res) => { // Rota raiz
   var nome = req.params.nome; // Obtém o parâmetro 'nome' da URL
   var lang = req.params.lang; // Obtém o parâmetro 'lang' da URL
+  var exibirMsg = false; // Obtém o parâmetro 'isErro' da URL
+  var produtos = [
+    {nome: "TV", preco: 2200.00},
+    {nome: "Geladeira", preco: 3200.00},
+    {nome: "Mouse", preco: 80.00},
+    {nome: "Teclado", preco: 150.00}
+  ]
   res.render('index', {
     nome: nome,
     lang: lang,
     empresa: "Curso de Node.js",
-    inscritos: 8000
+    inscritos: 8000,
+    msg: exibirMsg,
+    produtos: produtos
   }); // Renderiza a view 'index.ejs'
 });
 
