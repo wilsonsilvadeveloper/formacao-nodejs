7686c6c5469f1dae4fc09d62532b4cc93eef9ab5
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2025-10-24 16:23:16 -0300

    configura o servidor Express para usar EJS como motor de visualização e serve arquivos estáticos da pasta 'public'

--- projeto01/index.js
+++ projeto01/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 app.listen(port, () => {
   console.log(`Servidor nodeJS rodando na porta ${port}`);
 });
