9a897c312e47a0ec92b0257452bb515ba1e991aa
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2025-10-24 16:54:05 -0300

    adiciona rota para renderizar a página de perguntas

--- projeto01/index.js
+++ projeto01/index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/perguntar', (req, res)=> {
+    res.render('perguntar');
+})
+
 app.listen(port, () => {
   console.log(`Servidor nodeJS rodando na porta ${port}`);
 });
