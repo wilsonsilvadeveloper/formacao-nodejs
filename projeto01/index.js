c3c47ed0afa891ef4220d3e0ce0ead7f1519ef2b
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2025-10-27 16:45:11 -0300

    adiciona body-parser para processamento de dados do formulário

--- projeto01/index.js
+++ projeto01/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index');
