b0d614e566f7e6cc5adb6978228152a030f52a25
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2025-10-30 09:47:57 -0300

    adiciona exportação do modelo Pergunta no arquivo Perguntas.js

--- projeto01/database/models/Perguntas.js
+++ projeto01/database/models/Perguntas.js
@@ -15,3 +15,5 @@ const Pergunta = connection.define('pergunta', {
 Pergunta.sync({force: false}).then(()=> {
     console.log('Tabela de perguntas criada com sucesso!');
 });
+
+module.exports = Pergunta;
